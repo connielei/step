22e93e4fb492c55ed1557a780277be052a0697b0
Connie Lei <callmecon328@gmail.com>
2020-05-28 14:37:18 -0400

    add user limit comment ability

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,7 +31,7 @@ function addRandomGreeting() {
  * Fetches a list of messages and adds it to the page.
  */
 async function getComments() {
-    const response = await fetch('/list-comments?num=10');
+    const response = await fetch(`/list-comments?num=${document.getElementById('num').value}`);
     const json = await response.json();
 
     const ulElement = document.getElementById('comments-list');
