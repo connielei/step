8a5805f35b4cc979def0c6bdeef91a5fff27433b
Connie Lei <callmecon328@gmail.com>
2020-06-08 13:46:44 +0000

    update 2019 to 2020, change method of finding empty comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -124,7 +124,7 @@ async function displayComments() {
         ulElement.appendChild(createListElement(comment));
     }
 
-    if (ulElement.textContent === '') {
+    if (json.length === 0) {
         ulElement.textContent = 'No comments yet.';
     }
 }
