9f5e5ecb0f47ca1550aae5a7d729335500351d69
Connie Lei <callmecon328@gmail.com>
2020-06-15 21:44:11 +0000

    remove extra newline

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -20,7 +20,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
 import java.util.Iterator;
 
 public final class FindMeetingQuery {
